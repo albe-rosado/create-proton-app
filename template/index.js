e82d24050e1e328d36b93084f1bd55876ec003f4
Gustav Hansen <kusti8@gmail.com>
2018-03-01 13:02:34 -0500

    Update height and width props for Window

--- template/index.js
+++ template/index.js
@@ -6,7 +6,7 @@ class Example extends Component {
   render() { // all Components must have a render method
     return (
       <App> // you must always include App around everything
-        <Window title="Proton Native Rocks!" height={300} width={300} menuBar={false}>
+        <Window title="Proton Native Rocks!" size={{w: 300, h: 300}} menuBar={false}>
             // all your other components go here
         </Window>
       </App>
